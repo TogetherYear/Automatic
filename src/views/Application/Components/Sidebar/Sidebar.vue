43ea6eb4d3e5db367f9b391d49298ca77a6298bf
zhangwenjun <1346502061@qq.com>
2024-02-29 14:27:39 +0800

    Update:Clean

--- src/views/Application/Components/Sidebar/Sidebar.vue
+++ src/views/Application/Components/Sidebar/Sidebar.vue
@@ -17,16 +17,18 @@ instance.sidebar.Run()
 <template>
     <div class="Sidebar" @click.stop=""
         :style="{ transform: isShow ? 'scale(1.0)' : 'scale(0.0)', opacity: isShow ? '1.0' : '0.7' }">
-        <n-tooltip placement="top" trigger="hover" v-for="m in menus" :key="m.title">
-            <template #trigger>
-                <span class="Menu" @click="instance.sidebar.OnSwitchOptions(m)">
-                    <img :src="m.icon"
-                        :style="{ filter: currentOption == m.id ? 'hue-rotate(90deg) brightness(120%)' : 'hue-rotate(0deg)' }"
-                        alt="">
-                </span>
-            </template>
-            {{ m.title }}
-        </n-tooltip>
+        <span class="Menus">
+            <n-tooltip placement="top" trigger="hover" v-for="m in menus" :key="m.title">
+                <template #trigger>
+                    <span class="Menu" @click="instance.sidebar.OnSwitchOptions(m)">
+                        <img :src="m.icon"
+                            :style="{ filter: currentOption == m.id ? 'hue-rotate(90deg) brightness(120%)' : 'hue-rotate(0deg)' }"
+                            alt="">
+                    </span>
+                </template>
+                {{ m.title }}
+            </n-tooltip>
+        </span>
         <span class="Support">
             <img :src="supportIcon" alt="">
         </span>
